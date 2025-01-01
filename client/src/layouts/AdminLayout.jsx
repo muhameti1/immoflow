8ad553243c92f85648e5848da891c2cfd3a53431
muhameti1 <muhamethoxha123@gmail.com>
2025-01-01 22:30:11 +0100

    Update UI styles and components for improved Admin Dashboard experience

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -39,9 +39,7 @@ export default function AdminLayout({ children }) {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            {children}
-          </div>
+          <div className="">{children}</div>
         </div>
       </SidebarInset>
     </SidebarProvider>
