e18e6885cac703bda718239bb4f5f48aa488f597
muhameti1 <muhamethoxha123@gmail.com>
2025-01-03 10:36:40 +0100

    Add Settings link to app sidebar with corresponding icon

--- client/src/components/app-sidebar.jsx
+++ client/src/components/app-sidebar.jsx
@@ -14,6 +14,7 @@ import {
   Map,
   Megaphone,
   PieChart,
+  Settings,
   Settings2,
   SquareTerminal,
   Users,
@@ -95,6 +96,11 @@ const data = {
       title: "Mailings",
       url: "/app/mailings",
       icon: Megaphone,
+    },
+    {
+      title: "Settings",
+      url: "/app/settings",
+      icon: Settings,
     },
   ],
   projects: [
